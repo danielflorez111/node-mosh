851900ec1474470e464f0ebecee983a26ba0a472
Daniel Florez <Daniel.F.Florez.-ND@disney.com>
2020-03-01 21:00:07 -0500

    Mongo-demo finished

--- 05-mongo-demo/index.js
+++ 05-mongo-demo/index.js
@@ -16,18 +16,40 @@ const Course = mongoose.model('Course', courseSchema);
 
 async function createCourse() {
     const course = new Course({
-        name: 'Vue Course',
-        author: 'Laura Tejada',
-        tags: ['vue', 'frontend'],
+        name: 'Svelte',
+        author: 'Daniel Florez',
+        tags: ['Svelte', 'frontend'],
         isPublished: true
     });
+
     const result = await course.save();
     console.log(result);
 }
 
 async function getCourses() {
+    // comparison operators
+    // eq equal
+    // ne not equal
+    // gt greater than
+    // gte grater than or equal to
+    // lt less than
+    // lte less than or equal to
+    // in
+    // nin not in
+
+    // logical operators
+    // or
+    // and
+
+    // regex
+    // .find({ author: /^Daniel/ }) starts with
+    // .find({ author: /Daniel$/ }) ends with
+    // .find({ author: /.*Daniel.*/ }) contains
+
+
     const courses = await Course
-        .find({ author: 'Daniel Florez' })
+        .find({ name: 'Angular 9' })
+        // .find({ price: { $gt: 10, $lte: 20 } })
         .limit(10)
         .sort({ name: 1 })
         .select({ name: 1, author: 1 });
@@ -35,4 +57,23 @@ async function getCourses() {
     console.log(courses);
 }
 
+async function updateCourse(id) {
+    const course = await Course.findById(id);
+
+    if (!course) return;
+
+    course.isPublished = false;
+    course.author = 'Another Author Name';
+    const result = await course.save();
+    console.log(result);
+}
+
+async function removeCourse(id) {
+    const result = await Course.deleteOne({ _id: id });
+    console.log(result);
+}
+
+// createCourse();
+updateCourse('5e5594fb3881b41b18c8f697');
+removeCourse('5e5594fb3881b41b18c8f697');
 getCourses();
