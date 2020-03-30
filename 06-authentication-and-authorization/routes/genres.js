30ec3860d1d5b8a9564ebd09d0c3c5579fea6307
Daniel Florez <Daniel.F.Florez.-ND@disney.com>
2020-03-29 21:52:26 -0500

    Auth finished

--- 06-authentication-and-authorization/routes/genres.js
+++ 06-authentication-and-authorization/routes/genres.js
@@ -1,5 +1,6 @@
-const {Genre, validate} = require('../models/genre');
-const mongoose = require('mongoose');
+const { Genre, validate } = require('../models/genre');
+const auth = require('../middleware/auth');
+const admin = require('../middleware/admin');
 const express = require('express');
 const router = express.Router();
 
@@ -8,18 +9,18 @@ router.get('/', async (req, res) => {
   res.send(genres);
 });
 
-router.post('/', async (req, res) => {
-  const { error } = validate(req.body); 
+router.post('/', auth, async (req, res) => {
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   let genre = new Genre({ name: req.body.name });
   genre = await genre.save();
-  
+
   res.send(genre);
 });
 
 router.put('/:id', async (req, res) => {
-  const { error } = validate(req.body); 
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
@@ -27,11 +28,11 @@ router.put('/:id', async (req, res) => {
   });
 
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-  
+
   res.send(genre);
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', [auth, admin], async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
 
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
